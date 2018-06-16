fed5f6b26e1b86101d952395609cc1b24d848dca
Freeman <ejmorga1@gmail.com>
2018-06-16 10:28:16 -0400

    More javascript stuff

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,36 @@
 $(document).ready(function () {
 
 
-
-
-
+   
+   
+   
+    var start = "2018-02-05";
+    var end = "2018-05-10"; 
+    ///function launchLibinfo(start,end){
+        var xhr = $.get("https://launchlibrary.net/1.4/launch/" + start + "/" + end);
     
+        xhr.done(function(Info){
+            for (i=0; i<Info.launches.length; i++){
+                var results = $("<div class='result'>");
+                var loco  = Info.launches[i].location.name;
+                //var rocket = Info.launches[i].rocket.name;
+                var miss = Info.launches[i].name;
+                var img = Info.launches[i].rocket.imageURL;
+    
+                var lcI= Info.launches[i].location.infoURLs;
+                results.append(miss);
+                results.append(loco);
+                results.append(img);
+                console.log(loco);
+                console.log(miss);
+                console.log(lcI);
+            };
+        });
+    
+    
+    //}
+
+
+
+
 });
